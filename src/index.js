d715dd204afe6b2faa397c4bf0402df8914f1bb4
Vladislav <vlad.beznet@gmail.com>
2022-08-04 07:36:54 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.map(item => item.join(''));
+  let string = str;
+
+  if(string.length % 2 === 1) {
+    return false;
+  }
+
+  for(let i = 0; i < config.length;) {
+    let elem = config[i];
+
+    if(string.indexOf(elem) == -1) {
+      i++;
+    }
+    else {
+      string = string.replace(elem, '');
+      i = 0;
+    }
+  }
+
+  if(string === '') {
+    return true;
+  }
+  else return false;
 }
